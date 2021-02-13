b17569ccbc293eb4f4cd2f68254278bde4a81abe
Steve Tong <stevetong53@yahoo.com>
2021-02-13 11:35:40 -0800

    tailwind colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors,
   },
   variants: {
     extend: {},
