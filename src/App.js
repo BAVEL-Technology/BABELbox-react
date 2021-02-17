4c806bd2b16c3ca9ad97708be0d89a940efe5a66
Christian Payne <christianpayne522@gmail.com>
2021-02-16 21:25:31 -0800

    Updated .gitignore, added react-router-dom, added 404 page, added switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Wrapper from "./components/Wrapper";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/main";
+import AboutUs from "./pages/AboutUs";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <Router>
       <div>
         <Wrapper classes="h-screen w-screen bg-blue-200 flex flex-col justify-center">
-          <Route exact path="/" component={Main} />
+          {/* Switch statement for the router. */}
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/about-us" component={AboutUs} />
+            {/* Render this if no other page was found. */}
+            <Route component={PageNotFound} />
+          </Switch>
         </Wrapper>
+        
       </div>
     </Router>
   );
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PageNotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="">BABEL 404</h1>
+      </div>
+    );
+  }
+}
+
+export default PageNotFound;
