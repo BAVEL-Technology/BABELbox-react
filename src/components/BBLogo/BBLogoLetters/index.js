c75083a8326cde7b71cad611cede7b8b120a3fb6
Robert Schmahl <rschm007@gmail.com>
2021-02-16 20:22:19 -0800

    bblogo component complete

--- src/components/BBLogo/BBLogoLetters/index.js
+++ src/components/BBLogo/BBLogoLetters/index.js
@@ -1,9 +1,7 @@
 const BBLogoLetters = (props) => {
   return (
     <span
-      class={`inline-block duration-200 origin-bottom transform ease-in-out scale-75 mx-2 ${props.charColor}`}
-      onMouseOver="big(event)"
-      onMouseOut="small(event)"
+      class={`inline-block duration-100 origin-bottom scale-75 mx-2 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 ${props.charColor}`}
     >
       {props.character}
     </span>
