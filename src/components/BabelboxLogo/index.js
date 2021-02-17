79fdfc3249a59ffac41549cf93515b002e39aff1
Robert Schmahl <rschm007@gmail.com>
2021-02-16 20:06:00 -0800

    started main page components

---
+++ src/components/BabelboxLogo/index.js
@@ -0,0 +1,9 @@
+const BabelboxLogo = () => {
+    return (
+        <div className="brand text-center text-gray-700 font-semibold hidden lg:block md:block">
+            <span class="text-yellow-600 inline-block duration-200 origin-bottom transform ease-in-out scale-75 mx-2" onMouseOver="big(event)" onMouseOut="small(event)">B</span>
+        </div>
+    );
+};
+
+export default BabelboxLogo;
