b3fd5126f875a2638200173d63a44843f4e676d7
Christian Payne <christianpayne522@gmail.com>
2021-02-18 19:53:40 -0800

    Created lobby and liar liar outline.

---
+++ src/components/Lobby/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lobby extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+};
+
+export default Lobby;
---
+++ src/games/LiarLiar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Lobby from "../components/Lobby";
+
+class LiarLiar extends Component {
+  render() {
+    return (
+      <>
+        <Lobby/>
+      </>
+    );
+  }
+};
+
+export default LiarLiar;
