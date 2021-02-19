151f679b9e21e3e4520e943db6bed3568a85f96c
Steve Tong <stevetong53@yahoo.com>
2021-02-18 20:48:54 -0800

    play button for liarliar

--- src/games/LiarLiar/PlayButton/index.js
+++ src/games/LiarLiar/PlayButton/index.js
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
-const playButton = <FontAwesomeIcon icon={faPlay} />;
-
 class index extends Component {
   render() {
     return (
       <div>
         <button className="my-2 cursor-pointer lg:h-24 md:h-24 lg:w-24 md:w-24 w-16 h-16 rounded-full bg-green-400 flex items-center justify-center text-gray-100 hover:text-green-400 hover:bg-gray-100 border-4 border-green-400">
-          <playButton />
+          <FontAwesomeIcon icon={faPlay} />
         </button>
       </div>
     );
