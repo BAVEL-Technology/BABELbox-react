f8109938ded4bf28f6c51ed625eb85d4e0c4e19b
Christian Payne <christianpayne522@gmail.com>
2021-02-18 21:05:51 -0800

    Working on BB Logo and BB Logo Letters.

--- src/games/LiarLiar/index.js
+++ src/games/LiarLiar/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import BBLogo from '../../components/BBLogo';
 import Lobby from "./Lobby";
 
 class LiarLiar extends Component {
   render() {
     return (
       <>
+        <div className="position-absolute">
+          <BBLogo small={true}/>
+        </div>
         <Lobby/>
       </>
     );
