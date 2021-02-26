e274c5a87feb2a6ba5c736f8b516d0122c16c7be
Christian Payne <christianpayne522@gmail.com>
2021-02-25 21:18:52 -0800

    Added War button.

--- src/pages/main.js
+++ src/pages/main.js
@@ -28,9 +28,9 @@ function Main() {
           {/* War Game Button */}
           <GameButton
             classes="game-button w-full my-4 flex items-center justify-around bg-yellow-500 active:bg-yellow-600 text-gray-700 p-4 lg:text-5xl md:text-5xl text-3xl text-center rounded-xl tracking-widest shadow-lg duration-100 transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 cursor-pointer"
-            src="https://twemoji.maxcdn.com/v/13.0.1/72x72/1f925.png"
-            name="Liar Liar"
-            location="/liarliar"
+            // src="https://twemoji.maxcdn.com/v/13.0.1/72x72/1f925.png"
+            name="War"
+            location="/war"
           />
 
           {/* <PortalCodeCard
