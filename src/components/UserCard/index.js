6ea1a9ed67198ba79cf7b67f68c21c794c0fef55
Steve Tong <stevetong53@yahoo.com>
2021-03-02 21:21:38 -0800

    change leader function still need to work, star color doesnt work, it does work front wise console log

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,3 +1,4 @@
+import React, { useContext } from "react";
 import { contextType } from "react-copy-to-clipboard";
 import Modal from "../Modal";
 import LiarLiarContext from "../../games/LiarLiar/utils/LiarLiarContext";
@@ -23,17 +24,16 @@ const UserCard = (props) => {
   };
   const changeLeader = (userID) => {
     // TODO: After MVP
-    // const newLeader = await bb().update('portals', {portals: context.LiarLiarState.players.leader}, {
-
-    // });
-    const playerArray = context.LiarLiarState.players;
-    for (const i = 0; i < playerArray.length; i++) {
-      if (leader === true) {
+    console.log(context);
+    const playerArray = context.liarLiarState.players;
+    for (let i = 0; i < playerArray.length; i++) {
+      if (playerArray[i].leader === true) {
         playerArray[i].leader = false;
       } else if (playerArray[i].id === userID) {
         playerArray[i] = true;
       }
     }
+    console.log(playerArray);
   };
 
   const trashUser = (userID) => {
@@ -71,14 +71,16 @@ const UserCard = (props) => {
           }}
           className="make-leader w-8 h-8 rounded-full hover:bg-blue-300 flex items-center justify-center pl-1 cursor-pointer"
         >
-          <span className="fa-stack fa-1x user-leader">
-            <span className=" text-yellow-500 gold-star">
-              <i className="fas fa-star fa-stack-1x" aria-hidden="true"></i>
-            </span>
-            <span>
-              <i className="far fa-star fa-stack-1x" aria-hidden="true"></i>
-            </span>
-          </span>
+          <svg
+            className={`h-4 w-4 ${
+              props.user.leader ? "text-yellow-500" : "text-purple-500"
+            }`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+          </svg>
         </div>
         <div
           onClick={() => {
