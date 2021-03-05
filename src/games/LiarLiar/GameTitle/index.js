3045cbc23c9df5b98fc0f103e89e895984df67c3
Steve Tong <stevetong53@yahoo.com>
2021-03-05 13:28:49 -0800

    scoreboard implementation

--- src/games/LiarLiar/GameTitle/index.js
+++ src/games/LiarLiar/GameTitle/index.js
@@ -1,19 +1,24 @@
-import React, { Component } from "react";
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
+import Scoreboard from "../Scoreboard";
+import { useGame } from "games/LiarLiar/BabelBuilder/GameContext";
+const gameTitle = (props) => {
+  const [scoreOpen, setScoreOpen] = useState(false);
+  const gameState = useGame();
+  // console.log(this.props);
+  return (
+    <button
+      className={`${props.className}`}
+      onClick={() => {
+        setScoreOpen(!scoreOpen);
+        console.log("score open ", scoreOpen);
+      }}
+    >
+      <div className={`emoji`}>🤥</div>
+      <p className="font-extrabold game-button-p ml-5">{props.name}</p>
+      <Scoreboard hidden={!scoreOpen} users={gameState.players} />
+    </button>
+  );
+};
 
-class index extends Component {
-  render(props) {
-    // console.log(this.props);
-    return (
-          <button className={this.props.className}>
-            <div className={`emoji`}>
-              🤥
-            </div>
-            <p className="font-extrabold game-button-p ml-5">
-              {this.props.name}
-            </p>
-          </button>
-    );
-  }
-}
-
-export default index;
+export default gameTitle;
