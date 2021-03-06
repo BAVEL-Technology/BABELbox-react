386c0125380334a30319fe0cfd3aa59ef14926c8
Christian Payne <christianpayne522@gmail.com>
2021-03-05 20:27:12 -0800

    Timer starting to work.

--- src/games/LiarLiar/Components/Timer/index.js
+++ src/games/LiarLiar/Components/Timer/index.js
@@ -1,39 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
 const index = (props) => {
-  const [percentComplete, setPercentComplete] = useState(100);
+  const [percentComplete, setPercentComplete] = useState(0);
 
   const startTimeStamp = props.startTimeStamp;
-  const endTimeStamp = props.endTimeStamp;
+  const endTimeStamp = startTimeStamp + 30000;
 
-  console.log(`Props: \n`, props);
+  const timerStyles = {
+    width: percentComplete + '%'
+  }
 
-  console.log(`
-  Start Time: ${startTimeStamp}\n
-  End Time: ${endTimeStamp}\n
-  Percent Complete: ${percentComplete}
-  `);
+  console.log(timerStyles);
+
+  // console.log(`
+  // Time Difference: ${((1 - (startTimeStamp - Date.now())) / (endTimeStamp - startTimeStamp)) * 100}
+  // `);
+
+  const calculatePercentage = () => {
+    const percentage = Math.floor(((1 - (startTimeStamp - Date.now())) / (endTimeStamp - startTimeStamp)) * 100);
+    // console.log(percentage);
+    return percentage;
+  }
   
   
   useEffect(() => {
-    const timer = setInterval(()=>{
-      setPercentComplete((endTimeStamp - Date.now()) / (endTimeStamp - startTimeStamp));
-
-      console.log(`Inside Timer Percent Complete: ${percentComplete}`);
+    const timer = setTimeout(()=>{
       
+      setPercentComplete(calculatePercentage());
+
       if(percentComplete >= 100)
       {
-        console.log("Timer complete.");
+        setPercentComplete(100);
         clearInterval(timer);
       }
     }, 1000);
-  }, []);
+
+    return () => clearTimeout(timer);
+  });
 
   return (
-    <div className="relative z-50">
+    <div className="relative z-50 w-full">
       {/* Foreground */}
-      <div className="absolute h-12 bg-green-500 rounded-full z-30 transition-all ease-in-out duration-150" style={{
-      width: `${percentComplete}%`}}></div>
+      <div className="absolute h-12 bg-green-500 rounded-full z-30 transition-all ease-in-out duration-150" style={timerStyles}>
+        Time Left
+      </div>
       {/* Background */}
       <div className="absolute w-full h-12 bg-blue-500 rounded-full z-20"></div>
     </div>
