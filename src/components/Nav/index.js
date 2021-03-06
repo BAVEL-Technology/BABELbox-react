ad1f33a59e610cff7472eb23455297187de6efd2
Christian Payne <christianpayne522@gmail.com>
2021-03-05 22:28:03 -0800

    Updated styles of about us page and changed the nav bar.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -24,36 +24,39 @@ const Navbar = (props) => {
       <ul
         className={`flex items-center font-semibold cursor-pointer space-x-7 text-${props.color} font-roboto font-medium text-sm`}
       >
+      <li
+        className={`dropdown inline-block relative transition duration-500 ease-in-out transform hover:scale-110 hover:-translate-y-1 ${styles.navButton} ${styles.hoverTransform}`}
+      >
+        Games
+        <ul>
+          <li className="dropdown-menu absolute hidden text-base pt-5">
+            <a
+              href="/liarliar"
+              style={{ fontFamily: "Sniglet" }}
+              className="px-4 py-2 rounded-md bg-white text-indigo-800 focus:outline-white"
+            >
+              LiarLiar
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <a href="/about-us">
+        <li className={`${styles.navButton} ${styles.hoverTransform}`}>
+          About
+        </li>
+      </a>
+      
       <a href="/help">
         <li className={`${styles.navButton} ${styles.hoverTransform}`}>
           Help
         </li>
-        </a>
+      </a>
 
-        <a href="/about-us">
-        <li className={`${styles.navButton} ${styles.hoverTransform}`}>
-          About
-        </li>
-        </a>
 
-        <li
-          className={`dropdown inline-block relative transition duration-500 ease-in-out transform hover:scale-110 hover:-translate-y-1 ${styles.navButton} ${styles.hoverTransform}`}
-        >
-          Games
-          <ul>
-            <li className="dropdown-menu absolute hidden text-base pt-5">
-              <a
-                href="/liarliar"
-                style={{ fontFamily: "Sniglet" }}
-                className="px-4 py-2 rounded-md bg-white text-indigo-800 focus:outline-white"
-              >
-                LiarLiar
-              </a>
-            </li>
-          </ul>
-        </li>
+        
 
-        <li className={`${styles.navButton} ${styles.hoverTransform}`}>
+        {/* <li className={`${styles.navButton} ${styles.hoverTransform}`}>
           Login
         </li>
 
@@ -62,7 +65,7 @@ const Navbar = (props) => {
           className={`p-2 border-2 border-${props.color} rounded-3xl flex items-center justify-center hover:bg-white ${styles.hoverTransform}`}
         >
           Sign Up
-        </li>
+        </li> */}
         
       </ul>
     </div>
